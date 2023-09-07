af26d6af735beb21600a4a5ffbcdcb8d344f8230
ian otieno <ianotieno23@gmail.com>
2023-09-08 00:34:38 +0300

    update

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { TaskService } from 'src/app/services/task.service';
 import { Task } from 'src/app/Task';
-import { TASKS } from 'src/app/mock-task';
+
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
@@ -9,9 +10,9 @@ import { TASKS } from 'src/app/mock-task';
 export class TasksComponent implements OnInit {
    tasks:Task[]=[];
 
-  constructor(){}
+  constructor(private taskService:TaskService){}
   ngOnInit(): void {
-    
+    this.tasks = this.taskService.getTask()
   }
 
 }
