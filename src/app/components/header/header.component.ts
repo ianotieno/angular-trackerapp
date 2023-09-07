4b9738908a0438ae903b8c49772cfb24a5ba3f7c
ian otieno <ianotieno23@gmail.com>
2023-09-07 23:23:31 +0300

    toggle

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit{
 
-  title :string= 'angular-project';
+  title :string= 'Task Tracker';
 
   constructor(){}
   ngOnInit(): void {
