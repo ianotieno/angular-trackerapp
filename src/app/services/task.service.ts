127241f303c279d2134e9ca936202c0abd9b9783
ian otieno <ianotieno23@gmail.com>
2023-09-08 01:44:02 +0300

    update

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -2,13 +2,16 @@ import { Injectable } from '@angular/core';
 import { Observable,of } from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-task';
+import {HttpClient,HttpHeaders} from '@angular/common/http' ;
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
 
-  constructor() { }
+  private apiUrl = 'http://localhost:5000/tasks'
+
+  constructor(private http:HttpClient) { }
   getTask():Observable<Task[]>{
     const tasks =of(TASKS);
     return tasks;
