eefbb3496a078c7e787fcd43681858a196ece672
ian otieno <ianotieno23@gmail.com>
2023-09-08 10:26:32 +0300

    add-task

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-add-task',
   templateUrl: './add-task.component.html',
   styleUrls: ['./add-task.component.css']
 })
-export class AddTaskComponent {
+export class AddTaskComponent implements OnInit{
+  constructor(){}
+  ngOnInit(): void {
+    
+  }
 
 }
