6a1348ecd31430e3496f4bb6eb800ebbf4ae2985
ian otieno <ianotieno23@gmail.com>
2023-09-08 11:30:34 +0300

    update

--- src/app/components/tasks/tasks.component.ts
+++ src/app/components/tasks/tasks.component.ts
@@ -22,6 +22,7 @@ export class TasksComponent implements OnInit {
   this.taskService.updateTaskReminder(task).subscribe()
  }
  addTask(task:Task){
-  console.log(task);
+  this.taskService.addTask(task).subscribe((task)=> (this.tasks.push(task)));
+  
  }
 }
